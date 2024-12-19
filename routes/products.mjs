b7e74a615c1615eaa61b494bad10225291dff598
Carlos207-750 <pl30qfr@eduvaud.ch>
2024-12-19 15:33:22 +0100

    ajouter des produit

--- routes/products.mjs
+++ routes/products.mjs
@@ -1,6 +1,6 @@
 import express from "express";
 import { products } from "../db/mock-product.mjs";
-import { success } from "./helper.mjs";
+import { success,getUniqueId } from "./helper.mjs";
 const productsRouter = express();
 productsRouter.get("/", (req, res) => {
     const message = "La liste des produits a bien été récupérée.";
@@ -12,6 +12,22 @@ productsRouter.get("/:id", (req, res) => {
     const product = products.find((product) => product.id == productId);
     const message = `Le produit dont l'id vaut ${productId} a bien été récupéré.`;
     res.json(success(message, product));
+});
+
+
+
+productsRouter.post("/", (req, res) => {
+// Création d'un nouvel id du produit
+// Dans les prochains versions, c'est MySQL qui gérera cela pour nous (identifiant auto_increment)
+    const id = getUniqueId(products);
+// Création d'un objet avec les nouvelles informations du produits
+    const createdProduct = { ...req.body, ...{ id: id, created: new Date() } };
+// Ajout du nouveau produit dans le tableau
+    products.push(createdProduct);
+// Définir un message pour le consommateur de l'API REST
+    const message = `Le produit ${createdProduct.name} a bien été créé !`;
+// Retourner la réponse HTTP en json avec le msg et le produit créé
+    res.json(success(message, createdProduct));
 });
 
 export { productsRouter }
--- src/app.mjs
+++ src/app.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 const app = express();
+app.use(express.json());
 const port = 3000;
 app.get("/", (req, res) => {
     res.send("API REST of self service machine !");
