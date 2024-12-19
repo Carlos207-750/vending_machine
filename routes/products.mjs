a0264022f1f03ec091c6d044156883388d6f842f
Carlos207-750 <pl30qfr@eduvaud.ch>
2024-12-19 14:26:22 +0100

    feat:route id

--- routes/products.mjs
+++ routes/products.mjs
@@ -6,4 +6,12 @@ productsRouter.get("/", (req, res) => {
     const message = "La liste des produits a bien été récupérée.";
     res.json(success(message, products));
 });
+
+productsRouter.get("/:id", (req, res) => {
+    const productId = req.params.id;
+    const product = products.find((product) => product.id == productId);
+    const message = `Le produit dont l'id vaut ${productId} a bien été récupéré.`;
+    res.json(success(message, product));
+});
+
 export { productsRouter }
