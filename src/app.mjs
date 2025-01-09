7ed47a24100fd02e7fb9eb5741d9ee9d1ce9d789
Carlos207-750 <pl30qfr@eduvaud.ch>
2025-01-09 15:37:39 +0100

    étape 5 et 6

--- src/app.mjs
+++ src/app.mjs
@@ -1,7 +1,17 @@
 import express from "express";
+import { sequelize, initDb } from "../db/sequelize.mjs"
 const app = express();
 app.use(express.json());
 const port = 3000;
+
+sequelize
+    .authenticate()
+    .then((_) =>
+        console.log("La connexion à la base de données a bien été établie")
+    )
+    .catch((error) => console.error("Impossible de se connecter à la DB"));
+initDb();
+
 app.get("/", (req, res) => {
     res.send("API REST of self service machine !");
 })
@@ -9,7 +19,8 @@ app.get("/api/", (req, res) => {
     res.redirect(`http://localhost:${port}/`);
 });
 import { productsRouter } from "../routes/products.mjs";
-app.use("/api/products", productsRouter);
+app.use("./api/products", productsRouter);
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
 });
+
