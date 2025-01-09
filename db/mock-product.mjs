0764be1bbaef28b7402628359127315bb914f00c
Carlos207-750 <pl30qfr@eduvaud.ch>
2025-01-09 13:46:16 +0100

    étape 4

--- db/mock-product.mjs
+++ db/mock-product.mjs
@@ -7,4 +7,38 @@ let products = [
     },
 
 ];
-export { products }
+/**
+ * Récupère le produit dont l'id vaut `productId`
+ * @param {*} productId
+ */
+const getProduct = (productId) => {
+    return products.find((product) => product.id == productId);
+};
+/**
+ * Supprime le produit dont l'id vaut `productId`
+ * @param {*} productId
+ */
+const removeProduct = (productId) => {
+    products = products.filter((product) => product.id != productId);
+};
+/**
+ * Met à jour le produit dont l'id vaut `productId`
+ * @param {*} productId
+ * @param {*} updatedProduct
+ */
+const updateProduct = (productId, updatedProduct) => {
+    products = products.map((product) =>
+        product.id == productId ? updatedProduct : product
+    );
+};
+/**
+ * Génère et retourne le prochain id des produits
+ * @param {*} products
+ */
+const getUniqueId = () => {
+    const productsIds = products.map((product) => product.id);
+    const maxId = productsIds.reduce((a, b) => Math.max(a, b));
+    const uniqueId = maxId + 1;
+    return uniqueId;
+};
+export { products, getProduct, removeProduct, updateProduct, getUniqueId };
