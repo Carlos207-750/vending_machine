ee8ae3f3f1f4dc368dbdbe5b8d30a7df1476dae0
Carlos207-750 <pl30qfr@eduvaud.ch>
2025-01-09 16:31:09 +0100

    étape 8

--- src/app.mjs
+++ src/app.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import { sequelize, initDb } from "../db/sequelize.mjs"
+import { productsRouter } from "../routes/products.mjs";
 const app = express();
 app.use(express.json());
 const port = 3000;
@@ -18,9 +19,14 @@ app.get("/", (req, res) => {
 app.get("/api/", (req, res) => {
     res.redirect(`http://localhost:${port}/`);
 });
-import { productsRouter } from "../routes/products.mjs";
-app.use("./api/products", productsRouter);
+app.use("/api/products", productsRouter);
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
+});
+
+app.use(({ res }) => {
+    const message =
+        "Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.";
+    res.status(404).json(message);
 });
 
